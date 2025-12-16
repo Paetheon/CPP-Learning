5b395af15bbd55c1e382838a0fb8c16027a2b6d3
Timmy Ros <Ros.timmy.tr@gmail.com>
2025-12-15 21:30:59 -0800

    Add I/O code examples, notes, and .gitignore file

---
+++ CPP Version Check/check_version.cpp
@@ -0,0 +1,6 @@
+#include <print> // C++23 header
+
+int main() {
+    std::println("Hello, C++23 Standard Library!"); 
+    return 0;
+}
---
+++ redo.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    int temp = 5;
+    std::cout << temp << std::endl;
+    return 0;
+}
